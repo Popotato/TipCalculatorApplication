16d0d5da03746c458e1dcd45a3f715539d5d0b7b
Ben Stix <ud98@me.com>
2017-09-27 15:33:10 -0400

    Declared initial variables. -- also added imports

--- app/src/main/java/com/example/cstuser/tipcalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/cstuser/tipcalculatorapp/MainActivity.java
@@ -1,9 +1,22 @@
 package com.example.cstuser.tipcalculatorapp;
 
+import android.app.Activity;
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat = NumberFormat.getPercentInstance();
+
+    private double billAmount = 0.0;
+    private double percent = 0.15;
+    private TextView amountTextView;
+    private TextView percentTextView;
+    private TextView tipTextView;
+    private TextView totalTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
